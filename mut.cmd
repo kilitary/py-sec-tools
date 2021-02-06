59f8c10fafe21df687e3a247ae5030df6cdeeb66
kilitary <kilitary@gmail.com>
2021-02-06 21:22:59 +0300

    push

--- mut.cmd
+++ mut.cmd
@@ -1,42 +1,22 @@
 @ECHO ===========================================================
-ECHO 54pol
-:Roberta
-GOTO :Cordula
-:Susannah
-SET Inna[165]="l371iakzac1tm0AfAv6mc2q25dq5l2"
-:Cordula
-:Beverie
-SET Corrinne="a1f1hl3cc2g80d dk1i5"
-GOTO :Susannah
-:Aviva
-REM lqcax6pt862zhmg
-ECHO qzqp9
-:Janka
-:Celka
-GOTO :Janka
-TITLE 2o33sytv
-ECHO 87kei
-:Hildagard
-:Catrina
-GOTO :Beverie
-REM uqtxv
-TITLE yx4gru3o
-GOTO :Hildagard
-ECHO irr6l
-ECHO 2nAqt
-GOTO :Roberta
-SET Olwen[218]="7rku1bt b"
-ECHO 8AcyA
-GOTO :Catrina
-SET Marcy[174]="zp 37 he8a"
-TITLE piu092ig
-GOTO :Aviva
-ECHO n5wqp
-SET Maudie="z8ewvqp2hieo5orjenrp8llua4jrc2"
-:Lusa
-GOTO :Celka
-ECHO 44jkb
-REM cxdqxv s5fmex3
-GOTO :Lusa
-TITLE h3tq0zfz
+REM bnurvd4yt9r ays8lsvl
+:Debra
+GOTO :Debra
+TITLE 95Apdmxe
+:Marcia
+:Wilhelmine
+:Wendy
+ECHO xtjxq
+GOTO :Wendy
+SET Margot="ti36ufz4hrvtmrdg0uo"
+:Nanete
+REM hx2lrkvjvnobotz
+GOTO :Marcia
+TITLE bcf5u7Ae
+REM 36vgvAcqgs4od33
+GOTO :Wilhelmine
+REM 8 8 m
+ECHO udoyv
+GOTO :Nanete
+SET Jandy[113]="ewd4odrggyi83fb9w o"
 EXIT
