734f102f0993953fdcdacb506ec8b1410cf2ab71
kilitary <kilitary@gmail.com>
2021-02-13 06:11:29 +0300

    push

--- genbat.py
+++ genbat.py
@@ -44,52 +44,52 @@ class Assember(object):
 			choose = secrets.choice([1, 2, 3, 4, 5])
 
 			if choose == 1:
-				self.push(Command(Operand.ECHO, Randomer.str_id_generator(size=5)))
+				self.push_command(Command(Operand.ECHO, Randomer.str_id_generator(size=5)))
 			elif choose == 2:
-				self.push(Command(Operand.TITLE, Randomer.str_id_generator(size=8)))
+				self.push_command(Command(Operand.TITLE, Randomer.str_id_generator(size=8)))
 			elif choose == 3:
-				self.push(Command(Operand.REM, Randomer.str_str_generator(size=5)))
+				self.push_command(Command(Operand.REM, Randomer.str_str_generator(size=5)))
 			elif choose == 4:
-				self.push(Command(Operand.SET, f"{Randomer.rnd_name()}=\"{Randomer.str_str_generator(size=10)}\""))
+				self.push_command(Command(Operand.SET, f"{Randomer.rnd_name()}=\"{Randomer.str_str_generator(size=10)}\""))
 			elif choose == 5:
-				self.push(Command(Operand.SETV, f"{Randomer.rnd_name()}[{random.randint(0, 2500)}]=\"{Randomer.str_str_generator(size=10)}\""))
+				self.push_command(Command(Operand.SETV, f"{Randomer.rnd_name()}[{random.randint(0, 2500)}]=\"{Randomer.str_str_generator(size=10)}\""))
 
 		return
 
 	def save(self) -> None:
-		self.write(self.filename, self.join_code())
+		#var_dump(self.command_pipeline)
+		self.write(self.filename, self.get_code())
 
 	def sort(self) -> None:
 		self.command_pipeline = sorted(self.command_pipeline, key=lambda command: command.offset)
 
-	def join_code(self) -> str:
+	def get_code(self) -> str:
 		code = ''
 
 		self.sort()
 
-		#code = '\n'.join(self.command_pipeline)
 		for command in self.command_pipeline:
 			code += str(command).strip() + f"\n"
 
 		return code.strip()
 
 	def get_operand_at(self, offset):
-		operand = [x for x in self.command_pipeline if x.offset == offset]
-		return operand[0].operand if len(operand) > 0 else None
+		rec = [x for x in self.command_pipeline if x.offset == offset]
+		return rec[0].operand if len(rec) > 0 else None
 
 	def num_commands(self) -> int:
 		return len(self)
 
-	def push(self, command, offset=0) -> int:
+	def push_command(self, command, offset=0) -> int:
 		if offset == -1:
 			max = self.num_commands() - 1 if self.num_commands() - 1 > 0 else 0
 			command.offset = random.randint(0, max)
-		# deb(f"push {command} @ {command.offset} ({self.get_operand_at(command.offset)})")
+			#deb(f"push {command} @ {command.offset} ({self.get_operand_at(command.offset)})")
 		else:
-			command.offset = offset if offset else self.offset
+			command.offset = offset if offset else self.offset + 1
 
 		self.command_pipeline.append(command)
-		self.offset += offset
+		self.offset = command.offset
 
 		if ip_debug:
 			deb(f'{self.offset:04d} {command} ({len(assember)})')
@@ -129,16 +129,16 @@ class Command(object):
 if __name__ == '__main__':
 	offset = 0
 	assember = Assember("mut.cmd")
-	max = max_operands = random.randint(11, 21)
+	max = max_operands = random.randint(4, 11)
 
-	num_gotos = random.randint(5, max)
+	num_gotos = random.randint(1, max)
 
 	ii = ['\\', '|', '/', '-']
 	numIi = 0
 	ip = 0
 
-	for ng in range(num_gotos):
-		assember.junk(min=1, max=13)
+	#for ng in range(num_gotos):
+	#	assember.junk(min=1, max=4)
 
 	for step in range(0, max_operands - 1):
 		if max >= 1611:
@@ -153,14 +153,14 @@ if __name__ == '__main__':
 		# assember.junk(min=1, max=0)
 		# print(f'new ip: {ip}')
 		label = Randomer.str_generator()
-		ip = assember.push(Command(Operand.GOTO, ":" + label), offset=-1)
+		ip = assember.push_command(Command(Operand.GOTO, ":" + label), offset=-1)
 		# assember.junk(min=1, max=0)
 		# print(f'new ip: {ip}')
-		ip = assember.push(Command(Operand.LABEL, ":" + label), offset=-1)
+		ip = assember.push_command(Command(Operand.LABEL, ":" + label))
 
 		if step == max - 2:
-			assember.push(Command(Operand.EXIT))
+			assember.push_command(Command(Operand.EXIT))
 
-	deb(f"{assember.join_code()}")
+	deb(f"{assember.get_code()}")
 
 	assember.save()
