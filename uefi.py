0bc13cb7931c88136ac227768a8c75ba7393bc15
Steve Stagg <stestagg@gmail.com>
2022-10-02 15:33:04 +0300

    work

--- uefi.py
+++ uefi.py
@@ -1,7 +1,11 @@
+#  Copyright 2022 Sergey Efimov (kilitary@gmail.com)
+
 import uefi_firmware
-with open('D:\\temp\\bios_f51\B450MS2H.F51', 'r') as fh:
-	file_content = fh.read()
+
+
+with open('D:\\temp\\bios_f51\\B450MS2H.F51', 'r') as fh:
+    file_content = fh.read()
 parser = uefi_firmware.AutoParser(file_content)
 if parser.type() != 'unknown':
-	firmware = parser.parse()
-	firmware.showinfo()
+    firmware = parser.parse()
+    firmware.showinfo()
